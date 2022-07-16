8e47d49837b65949325f4c78de538ba08047aa43
Su <46551151+ktSuW@users.noreply.github.com>
2022-07-16 22:55:54 +1000

    Time: 1 ms (78.95%), Space: 43.5 MB (21.17%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -11,28 +11,41 @@
 
 /*
 Can solve this problem using either iterative or recursive approach. 
+// Recursive Approach 
+//----------------------------
 1. Base case list1 is null , list2 is null
 2. Walk towards the base case
     - list1.val < list2.val , then move list1 node to list1.next and keep comparing with list2
     - else 
         move list2
-3. Vistualise recursive call stack
-    
+
+Time O(n + m)
+Space O(m + n) due to recursive call stack use 
+
 */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        // base case 
-        if(l1 == null) return l2;
-        if(l2 == null) return l1;
-        // walk towards the base case
-        if(l1.val < l2.val){
-   
-            l1.next =  mergeTwoLists(l1.next, l2);
-            return l1;
-        }else{
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
+        // dummy node is to keep head address 
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                curr.next = l1;
+                l1 = l1.next;
+            }else{
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
         }
- 
+        
+        // There might be nodes left 
+        // to cover this case
+        if(l1 == null){
+            curr.next = l2;
+        }else{
+            curr.next = l1;
+        }
+        return dummy.next;
     }
 }
