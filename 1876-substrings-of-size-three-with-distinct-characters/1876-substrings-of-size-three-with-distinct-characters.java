e3128d0d42ff7760a9d0df0af1ac784513b6572b
Su <46551151+ktSuW@users.noreply.github.com>
2022-07-20 21:49:54 +1000

    Time: 2 ms (64.50%), Space: 42.4 MB (41.95%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -29,7 +29,7 @@ Option 1
 */
 // Time O(n)
 // Space O(n)
-class Solution {
+/*
     public int countGoodSubstrings(String s) {
  
         int result = 0;
@@ -42,6 +42,22 @@ class Solution {
             if(set.size() ==3) result++;
             else{
                 set.remove(0);
+            }
+        }
+        return result;
+    }
+*/
+class Solution {
+    public int countGoodSubstrings(String s) {
+ 
+        int result = 0;
+        
+        for(int i = 0; i < s.length() - 2; i++){
+            if(s.charAt(i) != s.charAt(i+1) &&
+                s.charAt(i+1) != s.charAt(i+2) &&
+               s.charAt(i) != s.charAt(i+2)
+              ){
+                result++;
             }
         }
         return result;
