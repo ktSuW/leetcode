18080dd160c2ecbaa1d56ca97f402d188f22da95
Su <46551151+ktSuW@users.noreply.github.com>
2022-07-20 21:42:34 +1000

    Time: 6 ms (25.99%), Space: 44.4 MB (9.38%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,14 +1,21 @@
+// Time O(n)
+// Space O(n)
 class Solution {
     public int countGoodSubstrings(String s) {
  
         int result = 0;
-        
         // Add -2 to the length() to ensure end can move till the end of the array
-        for(int i=0;i<s.length()-2;i++){
+        // Use two pointers approach to cover for length of three
+        // start pointer 
+        // end pointer => they both need to start from the same point 
+        for(int start =0; start<s.length()-2; start++){
             // if(i+3 > s.length()) break;
+            // Since set is initialised inside start, every time, new set will be initialised, and empty set.
+            // Therefore, we don't need to worry about clearing the set for each end iteration
+            // However, it could imapct on the memory 
             Set<Character> set = new HashSet<>();
-            for(int j=i;j<i+3;j++){
-                set.add(s.charAt(j));
+            for(int end= start; end < start+3;end++){
+                set.add(s.charAt(end));
             }
             if(set.size()==3){
                 result++;
