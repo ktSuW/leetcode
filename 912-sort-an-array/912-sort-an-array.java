81d23ab9b518d83b58c9617b4eb9d98c2032de66
Su <46551151+ktSuW@users.noreply.github.com>
2022-07-26 19:35:43 +1000

    Time: 15 ms (61.94%), Space: 70.8 MB (29.50%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,3 +1,44 @@
+/*  
+        low = l
+        high = h
+        p = pivot
+        ------------------
+        
+        l    p          h
+        5   4   3   2   1
+        
+            l       h
+        1   4   3   2   5
+        
+                l,h
+        1   2   3   4   5
+        
+        Now, pivot is in correct position
+
+    1   2   3   
+        
+        How to find the violation condition
+        while nums[l] < pivot , keep moving
+        while nums[h] > pivot , keep moving - decrease e
+        
+      start     l,h     end
+        1   2   3   4   5
+        
+        For recursive partitioning
+    
+    start       h   l end
+        1   2   3   4   5
+        
+        range will be 
+            => start to high
+            => low to end 
+        
+  Time (nlogn) - Average case
+        n^2    - worst case
+
+    space O(n) in place swapping => compared to Merge sort
+
+*/
 class Solution {
     public int[] sortArray(int[] nums) {
         if(nums.length== 0)return nums;
@@ -6,39 +47,33 @@ class Solution {
     }
     
     public void quickSort(int[] nums, int low, int high){
-        if(high <= low)
-			return ;
-		int middle= low + (high-low)/2;
-		int pivot = nums[middle];
-		int i=low , j=high;
-		
-		while( i<=j ) {
-			
-			while(nums[i] < pivot)
-				i++;
-			
-			while(nums[j] > pivot)
-				j--;
-			
-			if(i<=j) {
-				//SWAP
-				swap(nums, i, j);
-				i++;
-				j--;
-			}
-			
-		}
-		
-		if(low < j)
-			quickSort(nums,low,j);
-		
-		if(i<high)
-			quickSort(nums,i,high);
-    }
-    
-    private void swap(int[] nums, int low, int high){
-        int temp = nums[low];
-        nums[low] = nums[high];
-        nums[high] = temp;
+        if(low >= high) return;
+        int s = low;
+        int e = high;
+        int mid = s + (e - s)/2;
+        int pivot = nums[mid];
+        
+        while(s <= e){
+            while( s<= e && nums[s] < pivot){
+                s++;
+            }
+            while(s<= e && nums[e] > pivot){
+                e--;
+            }
+            
+            if(s <= e){
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+        // now the pivot is at the correct index
+        // sort two half 
+        quickSort(nums, low, e);
+        quickSort(nums, s, high);
+        
     }
 }
+
