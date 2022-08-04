e3b8af3b947fefa1932ebeb6d36c368b2e634e6d
Su <46551151+ktSuW@users.noreply.github.com>
2022-08-04 20:58:49 +1000

    Time: 1 ms (51.87%), Space: 40.3 MB (95.61%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -16,34 +16,45 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         // Iterative approach 
-//         List<Integer> result = new ArrayList<>();
-//         Stack<TreeNode> stack = new Stack<>();
-//         if(root == null) return result;
+        // preorder : visit order - root , left , right
+        // Will use stack. LIFO
+        // Therefore, right node must be added to the stack first so that I can visit left node
+        // Time O(n)
+        // Space O(n) due to stack data structure 
+        List<Integer> preorder = new ArrayList<>();
+        if(root == null) return preorder;
         
-//         stack.add(root);
-//         while(!stack.isEmpty()){
-//             TreeNode node = stack.pop();
-//             if (node != null) {
-//             result.add(node.val);
-//             stack.add(node.right);
-//             stack.add(node.left);
-//         }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        
+        while(!stack.isEmpty()){
+            root = stack.pop();
+            preorder.add(root.val);
             
-
-//         }
-//         return result;
+            if(root.right != null){
+                stack.push(root.right);
+            }
+            
+            if(root.left != null){
+                stack.push(root.left);
+            }
+        }
+        return preorder;
         
+        //=========================================
+        // Time O(n)
+        // Space O(n)
         // Recursive approach 
-        List<Integer> result = new ArrayList<>();
-        helper(result, root);      
-        return result;
-    }
+//         List<Integer> result = new ArrayList<>();
+//         helper(result, root);      
+//         return result;
+//     }
     
-    private List<Integer> helper(List<Integer> result, TreeNode root){
-        if (root == null) return result;
-        result.add(root.val);
-        helper(result, root.left );
-        helper(result, root.right);
-        return result;
+//     private List<Integer> helper(List<Integer> result, TreeNode root){
+//         if (root == null) return result;
+//         result.add(root.val);
+//         helper(result, root.left );
+//         helper(result, root.right);
+//         return result;
     }
 }
