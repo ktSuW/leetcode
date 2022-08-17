9968b223874497cd1ff309db81875469a6f983c4
Su <46551151+ktSuW@users.noreply.github.com>
2022-08-17 21:40:02 +1000

    Time: 2 ms (59.39%), Space: 53.9 MB (82.79%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -1,4 +1,23 @@
+/*
+First Approach
+=======================
+Use Two pointers approach
+Initialise headA to pointerA
+Intitialise headB to pointB -pB
 
+while pA != pB
+- if pA is not null. Move to the next node
+- else there is no more node in headA ListNode, therefore, assign headB to pA
+
+- if pB is not null, Move to the next node
+- else there is no more node in headB.
+
+- by the end of while loop, if there is no intersection, pA.next will be null. 
+Therefore it will give a correct answer
+Time complexity : O(n)
+Space Complexity : O(1)
+
+*/
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         //Two pointers appraoch 
