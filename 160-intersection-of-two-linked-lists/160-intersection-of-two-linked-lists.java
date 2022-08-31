8ee164931fbbeac09b0a3cead6ce2491805e8c27
Su <46551151+ktSuW@users.noreply.github.com>
2022-08-31 21:30:43 +1000

    Time: 2 ms (59.16%), Space: 53.7 MB (84.67%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -1,41 +1,46 @@
-/*
-First Approach
-=======================
-Use Two pointers approach
-Initialise headA to pointerA
-Intitialise headB to pointB -pB
-
-while pA != pB
-- if pA is not null. Move to the next node
-- else there is no more node in headA ListNode, therefore, assign headB to pA
-
-- if pB is not null, Move to the next node
-- else there is no more node in headB.
-
-- by the end of while loop, if there is no intersection, pA.next will be null. 
-Therefore it will give a correct answer
-Time complexity : O(n)
-Space Complexity : O(1)
-
-*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        //Two pointers appraoch 
-        ListNode pA = headA;
-        ListNode pB = headB;
-        while(pA != pB){
-            if(pA != null){
-               pA= pA.next; 
-            }else{
-                pA = headB;
-            }
-            if(pB != null){
-                pB = pB.next;
-            }else{
-                pB = headA;
-            }
+    private int findLength(ListNode node){
+        int length = 0;
+        while(node != null){
+            node = node.next;
+            length++;
         }
-        return pA;
+        return length;
     }
-}
+    
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // find the length of both linked lists
+        int a = findLength(headA);
+        int b = findLength(headB);
+        
+        while( a > b){
+            headA = headA.next;
+            a --;
+        }
+        
+        while(a < b){
+            headB = headB.next;
+            b--;
+        }
+        
+        while(headA != headB){
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+        
+    }
+    
 
+}
