72936aab7d65ab5fa76f2d82e49e8d150babc45a
Su <46551151+ktSuW@users.noreply.github.com>
2022-08-31 21:34:29 +1000

    Time: 2 ms (59.16%), Space: 55.6 MB (36.39%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -9,6 +9,16 @@
  *     }
  * }
  */
+/*
+Time O(n)
+Space O(1)
+ Length can be either same or different
+ 1. First find the length for each LL A and LL B
+ 2. if A length > B legnth, then move A. so that it will become align with A
+    if A length < B length, then move B.
+    Once they are align and they might still not at the at the intersection. So move both
+    return headA.
+*/
 public class Solution {
     private int findLength(ListNode node){
         int length = 0;
