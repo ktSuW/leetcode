3976314ffd456b98561805aa3f841574365ffe12
Su <46551151+ktSuW@users.noreply.github.com>
2022-09-09 22:11:40 +1000

    Time: 0 ms (100.00%), Space: 38.8 MB (98.66%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -5,11 +5,12 @@
         if(n <= 1) return n;
         return fib(n-1) + fib(n-2);
     }
+    //==============================================
     
-*/
-class Solution {
-    public int fib(int n) {
+        public int fib(int n) {
     // DP approach - memoisation
+    // Time complexity O(n)
+        // Space O(n) recursive call stack + O(n) for dp array 
         int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
         if(n <= 1) return n;
@@ -18,4 +19,37 @@ class Solution {
         }
         return dp[n] = fib(n-1) + fib(n-2);
     }
+    //==================================================
+    
+*/
+class Solution {
+    public int fib(int n) {
+    // Tabulation - bottom up
+    // try to go from base case and bottom to up
+        // Space O(n) - remove recursive stack space 
+        // Time O(n)
+        // check recurrence relation
+        // fib (n-1) + fib(n-2)
+        if(n <= 1) return n;
+        int[] save = new int[n+1];
+        save[0] = 0;
+        save[1] = 1;
+        for(int i = 2; i <=n; i++){
+            save[i] = save[i-1]+ save[i-2];
+        }
+        return save[n];
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
