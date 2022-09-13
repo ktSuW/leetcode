5e53b550d014f7a5e9fd42244630e8aa1be0569a
Su <46551151+ktSuW@users.noreply.github.com>
2022-09-13 20:47:41 +1000

    Time: 1 ms (90.27%), Space: 44.3 MB (16.61%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,23 +1,19 @@
 /*
-Greedy algorithms 
------------------------
-    // Greedy approach 
-    public int minCostClimbingStairs(int[] cost) {
-        for(int i  = 2; i < cost.length; i++){
-            cost[i] += Math.min(cost[i-1], cost[i-2]);
-        }
-        return Math.min(cost[cost.length -1], cost[cost.length -2]);
-    }
+Mixed of DP and greedy
+    0   1   2   3   4   5   6   7   8   9  <= index
+    1   100 1   1   1   100 1   1   100 1  <= values
+    s1  s2
+    cs = 1 + min(100,1) -> 1 = 2
     
-    
+        s1  s2
+
 */
 
 class Solution {
-    // Greedy approach 
     public int minCostClimbingStairs(int[] cost) {
         int step1 = 0;
         int step2 = 0;
-        for(int i = cost.length -1; i >= 0; i--){
+        for(int i = 0; i < cost.length ; i++){
             int currentStep = cost[i] + Math.min(step1, step2);
             step1 = step2;
             step2 = currentStep;
